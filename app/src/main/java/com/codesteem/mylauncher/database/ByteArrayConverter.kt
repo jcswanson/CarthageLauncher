b4a3c48cc10f6535342b0f5f67cd74523d6e2708
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:02 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/database/ByteArrayConverter.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/ByteArrayConverter.kt
@@ -2,6 +2,8 @@ package com.codesteem.mylauncher.database;
 
 import androidx.room.TypeConverter;
 
+import java.nio.charset.StandardCharsets;
+
 /**
  * A class that provides type conversion methods for storing byte arrays as strings in the Room database.
  * This is necessary because Room does not support storing byte arrays directly.
@@ -12,21 +14,21 @@ class ByteArrayConverter {
      * Converts a byte array to a string for storage in the Room database.
      *
      * @param byteArray The byte array to convert.
-     * @return A string representation of the byte array, or null if the input is null.
+     * @return A string representation of the byte array, or an empty string if the input is null.
      */
     @TypeConverter
-    fun fromByteArray(byteArray: ByteArray?): String? {
-        return byteArray?.let { String(it) }
+    public fun fromByteArray(byteArray: ByteArray?): String {
+        return byteArray?.let { String(it, StandardCharsets.UTF_8) } ?: ""
     }
 
     /**
      * Converts a string to a byte array for retrieval from the Room database.
      *
      * @param value The string to convert.
-     * @return A byte array representation of the string, or null if the input is null.
+     * @return A byte array representation of the string, or null if the input is an empty string.
      */
     @TypeConverter
-    fun toByteArray(value: String?): ByteArray? {
-        return value?.toByteArray()
+    public fun toByteArray(value: String?): ByteArray? {
+        return value?.let { value.toByteArray(StandardCharsets.UTF_8) }
     }
 }
