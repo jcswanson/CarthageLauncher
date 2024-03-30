d3b3e61dc36f2a9f219f5d7c3f89d0ebd7cac844
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:27 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/database/AppInfoConverter.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/AppInfoConverter.kt
@@ -4,13 +4,29 @@ import androidx.room.TypeConverter
 import com.codesteem.mylauncher.models.AppInfo
 import com.google.gson.Gson
 
+/**
+ * Converts AppInfo objects to and from strings, allowing them to be stored in the Room database.
+ */
 class AppInfoConverter {
 
+    /**
+     * Converts an AppInfo object to a JSON string, which can be stored in the Room database.
+     *
+     * @param appInfo The AppInfo object to convert.
+     * @return A JSON string representing the AppInfo object, or null if the input was null.
+     */
     @TypeConverter
     fun fromAppInfo(appInfo: AppInfo?): String? {
         return Gson().toJson(appInfo)
     }
 
+    /**
+     * Converts a JSON string to an AppInfo object, which can be used in the app.
+     *
+     * @param value The JSON string to convert.
+     * @return An AppInfo object representing the JSON string, or null if the input was null or
+     * invalid.
+     */
     @TypeConverter
     fun toAppInfo(value: String?): AppInfo? {
         return Gson().fromJson(value, AppInfo::class.java)
