c91d03a2348ed8675a895474f0e9eeeea1878f01
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:41 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/SelectionHiddenChangeListener.kt
+++ app/src/main/java/com/codesteem/mylauncher/SelectionHiddenChangeListener.kt
@@ -1,5 +1,10 @@
-package com.codesteem.mylauncher
-
+// Interface for listeners that want to be notified when the selection hidden state changes.
 interface SelectionHiddenChangeListener {
-    fun onSelectionHiddenChanged(selectedCount: Int,packageName:String)
+
+    // Called when the number of selected items and the package name have changed.
+    // This method should be implemented by the listener to handle the change in selection hidden state.
+    //
+    // @param selectedCount The new number of selected items.
+    // @param packageName The name of the package where the change occurred.
+    fun onSelectionHiddenChanged(selectedCount: Int, packageName: String)
 }
