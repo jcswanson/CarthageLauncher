a455e283e68c1fb7302f6bcbb6e60d7a5e89715f
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:20 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/models/AppSearchModel.kt
+++ app/src/main/java/com/codesteem/mylauncher/models/AppSearchModel.kt
@@ -2,20 +2,24 @@ package com.codesteem.mylauncher.models
 
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
-
-data class AppSearchModel(
-    // The name of the app or activity
-    val title: String? = "",
-
-    // The user-facing image for the app or activity
-    val userImage: Drawable,
-
-    // The package name of the app or activity
-    val packageName: String? = ""
-)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
 // This data class represents a model for an app or activity search result
 // It contains three properties:
 // - title: The name of the app or activity
 // - userImage: The user-facing image for the app or activity
 // - packageName: The package name of the app or activity
+
+@Parcelize
+data class AppSearchModel(
+    // The name of the app or activity
+    val title: String? = "",
+
+    // The user-facing image for the app or activity
+    val userImage: Bitmap? = null, // Changed to Bitmap for better memory management
+
+    // The package name of the app or activity
+    val packageName: String? = ""
+) : Parcelable
+
