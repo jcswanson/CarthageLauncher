c852c0d7fd91935f14a6c78c2021ee048eecfc2f
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:53 -0700

    comment

--- library/src/main/java/com/thesurix/gesturerecycler/transactions/MoveTransaction.kt
+++ library/src/main/java/com/thesurix/gesturerecycler/transactions/MoveTransaction.kt
@@ -1,33 +1,55 @@
 package com.thesurix.gesturerecycler.transactions
 
 /**
+ * A class that represents a move transaction for a generic type T.
+ * This class is used to move an item from one position to another within a list.
+ *
  * @author thesurix
  */
-class MoveTransaction<T>(private val from: Int,
-                         private val to: Int,
-                         private val headerEnabled: Boolean) : Transaction<T> {
+class MoveTransaction<T>(
+    /**
+     * The index of the item to be moved.
+     */
+    private val from: Int,
 
+    /**
+     * The index to which the item will be moved.
+     */
+    private val to: Int,
+
+    /**
+     * A flag indicating whether the list has a header or not.
+     * This is used to adjust the index of the moved item.
+     */
+    private val headerEnabled: Boolean
+) : Transaction<T> {
+
+    /**
+     * Performs the move transaction on the given [transactional] data.
+     * This method removes the item at the [from] index, adds it to the [to] index,
+     * and notifies the adapter of the move.
+     *
+     * @param transactional The data on which the transaction will be performed.
+     * @return True if the transaction was successful, false otherwise.
+     */
     override fun perform(transactional: Transactional<T>): Boolean {
         return with(transactional.data) {
+            // Remove the item at the from index
             val removedItem = removeAt(from)
+            // Add the removed item to the to index
             removedItem?.let {
                 add(to, it)
+                // Calculate the moved offset based on the headerEnabled flag
                 val movedOffset = if (headerEnabled) 1 else 0
+                // Notify the adapter of the move
                 transactional.notifyMoved(from + movedOffset, to + movedOffset)
+                // Return true if the transaction was successful
                 true
             } ?: false
         }
     }
 
-    override fun revert(transactional: Transactional<T>): Boolean {
-        return with(transactional.data) {
-            val removedItem = removeAt(to)
-            removedItem?.let {
-                add(from, it)
-                val movedOffset = if (headerEnabled) 1 else 0
-                transactional.notifyMoved(to + movedOffset, from + movedOffset)
-                true
-            } ?: false
-        }
-    }
-}
+    /**
+     * Reverts the move transaction on the given [transactional] data.
+     * This method removes the item at the [to] index, adds it to the [from] index,
+     * and notifies the adapter
