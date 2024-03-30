6c9e0fb25c454cdf17030ca0e54aac0783ff42cb
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:21 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/database/RuntimeExceptionAdapter.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/RuntimeExceptionAdapter.kt
@@ -1,9 +1,14 @@
-/**
- * A custom JSON serializer/deserializer for [RuntimeException] instances.
- * This adapter is used to serialize and deserialize RuntimeException objects
- * when converting data to and from JSON format using the Gson library.
- */
+import com.google.gson.*
+import java.lang.RuntimeException
+
 class RuntimeExceptionAdapter : JsonSerializer<RuntimeException>, JsonDeserializer<RuntimeException> {
 
-    /**
+    override fun serialize(runtimeException: RuntimeException, type: Type, jsonSerializationContext: JsonSerializationContext): JsonElement {
+        return jsonSerializationContext.serialize(runtimeException.message)
+    }
 
+    override fun deserialize(jsonElement: JsonElement, type: Type, jsonDeserializationContext: JsonDeserializationContext): RuntimeException {
+        val message = jsonDeserializationContext.deserialize<String>(jsonElement, String::class.java)
+        return RuntimeException(message)
+    }
+}
