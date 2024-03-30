59b18fa9fe237e28fba1b58dc64e06ee9d9cae94
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:42 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/bug_report/BugReportViewModel.kt
+++ app/src/main/java/com/codesteem/mylauncher/bug_report/BugReportViewModel.kt
@@ -14,36 +14,55 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 
+/**
+ * ViewModel for managing bug reports.
+ */
 class BugReportViewModel: ViewModel() {
+    // FirebaseFirestore instance for accessing the Firestore database.
     val db = FirebaseFirestore.getInstance()
+    
+    // Firebase storage instance for uploading media files.
     val storage = Firebase.storage
+    
+    // MutableStateFlow to hold the uploading status of media files.
     val uploadingStatus = MutableStateFlow<Resource<Boolean>>(Resource.Initial())
 
+    // MutableLiveData to hold the bug ID.
     private val _bugId = MutableLiveData<Int>()
+
+    // ArrayList to hold the URIs of bug media files.
     var bugMedia = ArrayList<Uri>()
 
+    /**
+     * Initialization block that gets the biggest ID in the database.
+     */
     init {
         getBiggestIdInTheDb()
     }
 
+    /**
+     * Function to upload a media file to Firebase storage and update the corresponding bug document in Firestore.
+     */
     private fun addBugMedia(media: Uri) = viewModelScope.launch {
         uploadingStatus.emit(Resource.Loading())
+        // Upload the media file to Firebase storage.
         storage.reference
             .child("$BUGS/${_bugId.value}/${media.lastPathSegment}")
             .putFile(media)
             .addOnSuccessListener {
+                // Get the download URL of the uploaded file.
                 storage.reference.
                 child("$BUGS/${_bugId.value}/${media.lastPathSegment}")
                     .downloadUrl
-                    .addOnSuccessListener {
-                        Log.e("images", it.toString())
+                    .addOnSuccessListener { uri ->
+                        Log.e("images", uri.toString())
+                        // Update the bug document in Firestore with the new media URL.
                         db.collection(BUGS)
                             .document(_bugId.value.toString())
                             .update(
                                 "media",
-                                FieldValue.arrayUnion(it.toString()),
+                                FieldValue.arrayUnion(uri.toString()),
                                 "media",
                                 FieldValue.arrayRemove("")
                             )
@@ -62,7 +81,9 @@ class BugReportViewModel: ViewModel() {
             }
     }
 
-
+    /**
+     * Function to add a new bug report to Firestore with the given title and description.
+     */
     fun addBug(
         title : String,
         description : String
@@ -72,65 +93,54 @@ class BugReportViewModel: ViewModel() {
             title,
             description
         )
+        // Get the latest bug document in the database.
         db.collection(BUGS)
             .orderBy("id", Query.Direction.DESCENDING).limit(1).get()
-            .addOnCompleteListener {
+            .addOnCompleteListener { task ->
                 viewModelScope.launch {
-                    if (it.isSuccessful && it.result?.metadata?.isFromCache == false) {
-                        if (it.result.isEmpty) {
+                    if (task.isSuccessful && task.result?.metadata?.isFromCache == false) {
+                        if (task.result.isEmpty) {
+                            // If the database is empty, set the bug ID to 1.
                             _bugId.value = 1
                             product.id = _bugId.value.toString()
+                            // Add the new bug document to Firestore.
                             db.collection(BUGS)
                                 .document(_bugId.value.toString())
                                 .set(product)
                                 .addOnSuccessListener {
                                     if (bugMedia.isEmpty()) {
+                                        // Emit Success if no media files are present.
                                         viewModelScope.launch {
                                             uploadingStatus.emit(Resource.Success(true))
                                         }
                                     }
                                 }
+                            // Upload all media files for the bug report.
                             bugMedia.forEach { media ->
                                 addBugMedia(media)
                             }
                         } else {
-                            it.result
-                            for (every in it.result!!) {
-                                _bugId.value = biggestId +1
-                                product.id = _bugId.value.toString()
-                                db.collection(BUGS)
-                                    .document(_bugId.value.toString())
-                                    .set(product)
-                                    .addOnSuccessListener {
-                                        if (bugMedia.isEmpty()) {
-                                            viewModelScope.launch {
-                                                uploadingStatus.emit(Resource.Success(true))
-                                            }
+                            // If the database is not empty, get the biggest ID and increment it.
+                            val listOfIds = ArrayList<Int>()
+                            task.result.documents.forEach { document ->
+                                listOfIds.add(document.id.toInt())
+                            }
+                            val biggestId = listOfIds.maxOrNull() ?: 0
+                            _bugId.value = biggestId + 1
+                            product.id = _bugId.value.toString()
+                            // Add the new bug document to Firestore.
+                            db.collection(BUGS)
+                                .document(_bugId.value.toString())
+                                .set(product)
+                                .addOnSuccessListener {
+                                    if (bugMedia.isEmpty()) {
+                                        // Emit Success if no media files are present.
+                                        viewModelScope.launch {
+                                            uploadingStatus.emit(Resource.Success(true))
                                         }
                                     }
-                                bugMedia.forEach { media ->
-                                    addBugMedia(media)
                                 }
+                            // Upload all media files for the bug report.
+                            bugMedia.forEach { media ->
+                                addBugMedia(media)
                             }
-                        }
-
-                    }
-                }
-            }.addOnCanceledListener {
-                Log.e("cancel", "")
-            }
-    }
-
-    private var biggestId = 0
-    private fun getBiggestIdInTheDb() = viewModelScope.launch {
-        val listOfIds = ArrayList<Int>()
-        db.collection(BUGS)
-            .get()
-            .addOnSuccessListener {
-                it.documents.forEach { document ->
-                    listOfIds.add(document.id.toInt())
-                }
-                biggestId = listOfIds.maxOrNull() ?: 0
-            }
-    }
-}
