c4be22b6c8776df1447d22f0099b4a6d17b990ce
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:48 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/database/ParcelableTypeAdapter.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/ParcelableTypeAdapter.kt
@@ -2,43 +2,52 @@ package com.codesteem.mylauncher.database
 
 import android.os.Parcel
 import android.os.Parcelable
+import com.google.gson.Gson
 import com.google.gson.TypeAdapter
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
 
 class ParcelableTypeAdapter<T : Parcelable>(private val creator: Parcelable.Creator<T>) : TypeAdapter<T>() {
+
+    private val gson = Gson()
+
     override fun write(writer: JsonWriter?, value: T?) {
-        throw UnsupportedOperationException("Serialization is not supported for Parcelable types.")
+        if (value == null) {
+            writer?.nullValue()
+            return
+        }
+
+        val parcel = Parcel.obtain()
+        value.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+
+        val parcelString = convertParcelToJson(parcel)
+        writer?.jsonValue(parcelString)
+
+        parcel.recycle()
+    }
+
+    private fun convertParcelToJson(parcel: Parcel): String {
+        val parcelable = creator.createFromParcel(parcel)
+        return gson.toJson(parcelable)
     }
 
     override fun read(reader: JsonReader?): T? {
-        if (reader != null) {
-            reader.beginObject()
-            while (reader.hasNext()) {
-                if (reader.nextName() == "CREATOR") {
-                    reader.beginObject()
-                    while (reader.hasNext()) {
-                        if (reader.nextName() == "createFromParcel") {
-                            reader.beginObject()
-                            while (reader.hasNext()) {
-                                if (reader.nextName() == "creator") {
-                                    return creator.createFromParcel(Parcel.obtain())
-                                } else {
-                                    reader.skipValue()
-                                }
-                            }
-                            reader.endObject()
-                        } else {
-                            reader.skipValue()
-                        }
-                    }
-                    reader.endObject()
-                } else {
-                    reader.skipValue()
-                }
-            }
-            reader.endObject()
+        if (reader == null) {
+            return null
         }
-        return null
+
+        val parcelString = reader.nextString()
+        val parcel = convertJsonToParcel(parcelString)
+
+        return creator.createFromParcel(parcel)
+    }
+
+    private fun convertJsonToParcel(parcelString: String): Parcel {
+        val parcel = Parcel.obtain()
+        parcel.writeString(parcelString)
+        parcel.setDataPosition(0)
+
+        return parcel
     }
 }
