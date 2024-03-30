9cbf4400b044e5665d10b579f0b27c6f9f0faa90
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:38 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/SelectionChangeListener.kt
+++ app/src/main/java/com/codesteem/mylauncher/SelectionChangeListener.kt
@@ -1,5 +1,18 @@
-package com.codesteem.mylauncher
+// com.codesteem.mylauncher package
 
+/**
+ * Interface for listening to changes in the user's selection.
+ *
+ * Implementors of this interface will receive notifications when the user's selection changes,
+ * including the number of items selected and the package name of the selected items.
+ */
 interface SelectionChangeListener {
-    fun onSelectionChanged(selectedCount: Int,packageName:String)
+
+    /**
+     * Called when the user's selection has changed.
+     *
+     * @param selectedCount The number of items currently selected.
+     * @param packageName The package name of the selected items.
+     */
+    fun onSelectionChanged(selectedCount: Int, packageName: String)
 }
