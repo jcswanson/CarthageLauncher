2102cd29dfb065218f27ba28bcbe15581ee4987f
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:17 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/database/converters/NotificationAction.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/converters/NotificationAction.kt
@@ -1,7 +1,12 @@
-package com.codesteem.mylauncher.database.converters
-
-import android.app.PendingIntent
-
+/**
+ * Data class representing the details of a notification action.
+ *
+ * @property title The title of the notification action. This should be a short, descriptive string
+ * that clearly communicates the purpose of the action.
+ * @property actionIntent The pending intent that will be triggered when the user selects this
+ * notification action. This should be an instance of [PendingIntent] that has been properly
+ * set up with the appropriate action and request code.
+ */
 data class NotificationActionDetails(
     val title: CharSequence?,
     val actionIntent: PendingIntent?
