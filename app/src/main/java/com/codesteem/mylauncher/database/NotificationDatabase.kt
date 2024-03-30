27ea33cde52f89792027292d52ffa0e4e64e3b46
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:13 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/database/NotificationDatabase.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/NotificationDatabase.kt
@@ -7,32 +7,17 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.codesteem.mylauncher.database.converters.StringListConverter
 
-
-/**
- * A database class that manages the [NotificationEntity] objects in the app.
- * This class is responsible for creating, reading, updating, and deleting [NotificationEntity] objects.
- */
 @Database(entities = [NotificationEntity::class], version = 1, exportSchema = false)
 @TypeConverters(StatusBarNotificationConverter::class, AppInfoConverter::class, StringListConverter::class)
 abstract class NotificationDatabase : RoomDatabase() {
 
-    /**
-     * Returns the [NotificationDao] object that provides methods for interacting with [NotificationEntity] objects.
-     */
     abstract fun notificationDao(): NotificationDao
 
-    /**
-     * A companion object that contains a nullable [NotificationDatabase] instance.
-     * This object also provides a method for getting the [NotificationDatabase] instance.
-     */
     companion object {
+
         @Volatile
         private var INSTANCE: NotificationDatabase? = null
 
-        /**
-         * Returns the [NotificationDatabase] instance for the given [context].
-         * If the instance is not created yet, it will be created and cached.
-         */
         fun getDatabase(context: Context): NotificationDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
