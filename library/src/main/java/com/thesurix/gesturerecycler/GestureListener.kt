421fcbfb5cd5e5dc5172231a5084b5518de0e919
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:29 -0700

    improve

--- library/src/main/java/com/thesurix/gesturerecycler/GestureListener.kt
+++ library/src/main/java/com/thesurix/gesturerecycler/GestureListener.kt
@@ -1,5 +1,8 @@
 package com.thesurix.gesturerecycler
 
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
 /**
  * The GestureListener class is a default gesture listener that handles manually spawned drag gestures.
  * It is designed to work with the ItemTouchHelper class from the Android Support Library.
@@ -11,16 +14,17 @@ package com.thesurix.gesturerecycler
  * @see GestureAdapter
  * @see ItemTouchHelper
  */
-class GestureListener(private val touchHelper: ItemTouchHelper) : GestureAdapter.OnGestureListener<Any> {
+class GestureListener(private val itemTouchHelper: ItemTouchHelper) :
+    GestureAdapter.OnGestureListener<RecyclerView.ViewHolder> {
 
     /**
      * Called when a drag gesture is started on a view holder. This method will delegate the call
      * to the ItemTouchHelper's startDrag method, which will initiate the drag and drop operation.
      *
      * @param viewHolder The view holder that the drag gesture was started on.
-     * @see ItemTouchHelper#startDrag(RecyclerView.ViewHolder)
+     * @see ItemTouchHelper.Callback#startDrag(RecyclerView.ViewHolder)
      */
-    override fun onStartDrag(viewHolder: GestureViewHolder<Any>) {
-        touchHelper.startDrag(viewHolder)
+    override fun onStartDrag(viewHolder: GestureViewHolder<RecyclerView.ViewHolder>) {
+        itemTouchHelper.startDrag(viewHolder.viewHolder)
     }
 }
