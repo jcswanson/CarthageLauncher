97a677aa0890020a9f5bc1e4adaa05a3fb5038cb
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:47 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/data/remote/models/Choice.kt
+++ app/src/main/java/com/codesteem/mylauncher/data/remote/models/Choice.kt
@@ -1,15 +1,27 @@
 // Data class representing a single choice in a list of possible responses.
 data class Choice(
-
-    // The amount of time the user has to respond, represented as a Delta object.
-    val delta: Delta,
+    // The amount of time the user has to respond.
+    val responseDeadline: Deadline,
 
     // The reason why this choice was the last one presented to the user, or null if it wasn't.
-    val finish_reason: String?,
+    val finishReason: String?,
 
     // The index of this choice in the list of possible responses.
-    val index: Int,
+    val choiceIndex: Int,
 
-    // The message associated with this choice, represented as a Message object.
-    val message: Message
+    // The message associated with this choice.
+    val associatedMessage: Message
 )
+
+// Extension property to convert a Delta object to a Deadline object
+val Delta.toDeadline: Deadline
+    get() = Deadline(this.value)
+
+// Data class representing a time duration.
+data class Delta(val value: Long)
+
+// Data class representing a time deadline.
+data class Deadline(val value: Long)
+
+// Data class representing a message.
+data class Message(val content: String)
