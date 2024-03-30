0e04948fb55a5fb841d6b5b98e20ec4f7867b36e
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:32 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/test/MonthHeaderViewHolder.kt
+++ app/src/main/java/com/codesteem/mylauncher/test/MonthHeaderViewHolder.kt
@@ -4,16 +4,15 @@ import android.widget.TextView
 import com.codesteem.mylauncher.databinding.MonthHeaderItemBinding
 import com.codesteem.mylauncher.gesture.GestureViewHolder
 
-
+/**
+ * A custom ViewHolder for displaying month headers in the launcher.
+ * This ViewHolder extends GestureViewHolder and overrides canDrag() and canSwipe() methods
+ * to disable drag and swipe gestures for month headers.
+ */
 class MonthHeaderViewHolder(binding: MonthHeaderItemBinding) : GestureViewHolder<MonthItem>(binding.root) {
 
+    // Initialize a TextView for displaying the month name
     private val headerText: TextView = binding.headerText
 
-    override fun canDrag() = false
+    /**
 
-    override fun canSwipe() = false
-
-    override fun bind(item: MonthItem) {
-        headerText.text = item.name
-    }
-}
