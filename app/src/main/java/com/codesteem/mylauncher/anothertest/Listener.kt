9becdc9c0e65c5619c2c296518e48444b6686a56
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:42 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/anothertest/Listener.kt
+++ app/src/main/java/com/codesteem/mylauncher/anothertest/Listener.kt
@@ -1,13 +1,27 @@
-package com.codesteem.mylauncher.anothertest
-
-import android.graphics.drawable.Drawable
-
 /**
- * Created by Azhar Rivaldi on 19-09-2020
+ * This interface defines a contract for a listener that can be used to show or hide various
+ * empty list views in the UI.
  */
-
 interface Listener {
+
+    /**
+     * Sets the visibility of the empty list top view.
+     *
+     * @param visibility `true` to show the empty list top view, `false` to hide it.
+     */
     fun setEmptyListTop(visibility: Boolean)
+
+    /**
+     * Sets the visibility of the empty list bottom view.
+     *
+     * @param visibility `true` to show the empty list bottom view, `false` to hide it.
+     */
     fun setEmptyListBottom(visibility: Boolean)
+
+    /**
+     * Sets the visibility of the empty list middle view.
+     *
+     * @param visibility `true` to show the empty list middle view, `false` to hide it.
+     */
     fun setEmptyListMid(visibility: Boolean)
 }
