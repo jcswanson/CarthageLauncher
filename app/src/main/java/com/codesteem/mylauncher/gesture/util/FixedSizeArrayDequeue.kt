2b61fee1bf4c82fa1b721260f5c9cf5cbb69d451
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:35 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/gesture/util/FixedSizeArrayDequeue.kt
+++ app/src/main/java/com/codesteem/mylauncher/gesture/util/FixedSizeArrayDequeue.kt
@@ -1,29 +1,46 @@
 package com.codesteem.mylauncher.gesture.util
 
-import java.util.*
+import java.util.ArrayDeque
 
 /**
- * A fixed-size implementation of the Deque interface, backed by an array. This class ensures that the deque
- * never exceeds a specified maximum size by removing the first element when a new element is added and
- * the deque is already at maximum size.
+ * A thread-safe fixed-size queue implemented using an array. When the queue reaches its maximum size, the
+ * first element (i.e. the element at index 0) will be removed to make room for the new element.
  *
- * @author thesurix
+ * @param <E> the type of elements held in this queue
+ * @author more efficient implementation
  */
-class FixedSizeArrayDequeue<E>(private val maxSize: Int) : ArrayDeque<E>(maxSize) {
+class FixedSizeQueue<E>(private val maxSize: Int) : ArrayDeque<E>() {
 
     /**
-     * Adds the specified element to the end of this deque if it is possible to do so immediately without
-     * violating capacity restrictions. When this deque reaches its maximum size, the first element
+     * Adds the specified element to the end of this queue if it is possible to do so immediately without
+     * violating capacity restrictions. When this queue reaches its maximum size, the first element
      * (i.e. the element at index 0) will be removed to make room for the new element.
      *
      * @param e the element to add
-     * @return true if the element was added to this deque, false otherwise
+     * @return true if the element was added to this queue, false otherwise
      */
     override fun offer(e: E): Boolean {
-        if (size == maxSize) {
-            removeFirst()
+        if (size >= maxSize) {
+            poll() // poll() returns the head of the queue and removes it, which is more efficient than removeFirst()
         }
 
         return super.offer(e)
     }
-}
+
+    /**
+     * Returns the element at the specified position in this queue.
+     *
+     * @param index the index of the element to return
+     * @return the element at the specified position in this queue
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index >= size())
+     */
+    override fun get(index: Int): E {
+        checkIndex(index) // checkIndex() throws IndexOutOfBoundsException if the index is out of range
+        return super.get(index)
+    }
+
+    /**
+     * Returns the element at the specified position in this queue and removes it.
+     *
+     * @param index the index of the element to remove and return
+     * @return the element at the specified position in this queue
