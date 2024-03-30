93af6ed72b12d5ded8af4b74df736195c1888081
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:42 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/anothertest/MainAdapter.kt
+++ app/src/main/java/com/codesteem/mylauncher/anothertest/MainAdapter.kt
@@ -2,110 +2,106 @@ package com.codesteem.mylauncher.anothertest
 
 import android.annotation.SuppressLint
 import android.content.ClipData
-import android.content.Context
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
-import android.view.View.DragShadowBuilder
-import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.bumptech.glide.Glide
-import com.codesteem.mylauncher.anothertest.MainAdapter.ListViewHolder
 import com.codesteem.mylauncher.databinding.ListItemMainBinding
 
-/**
- * Adapter for the main RecyclerView, which displays the grid of items.
- * 
- * Created by Usman on 19-09-2020
- */
+class MainAdapter(private val listener: Listener?) :
+    ListAdapter<Drawable, MainAdapter.ListViewHolder>(DiffCallback()) {
 
-class MainAdapter(var list: List<Drawable>,
-                  private val listener: Listener?,private val context: Context) : RecyclerView.Adapter<ListViewHolder>(), OnTouchListener {
+    private lateinit var context: Context
 
-    // Inflates the layout for each item in the RecyclerView
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val binding = ListItemMainBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        context = parent.context
         return ListViewHolder(binding)
     }
 
-    /**
-     * Binds the data to the views in the ViewHolder.
-     * Loads the drawable into the ImageView using Glide.
-     * Sets the onTouchListener and onDragListener for the CardView.
-     */
     @SuppressLint("ClickableViewAccessibility")
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        Glide.with(context).load(list[position]).into(holder.ivGrid)
-
-        holder.cvGrid.tag = position
-        holder.cvGrid.setOnTouchListener(this)
-        holder.cvGrid.setOnDragListener(DragListener(listener))
-    }
-
-    // Returns the size of the list
-    override fun getItemCount(): Int {
-        return list.size
-    }
-
-    // Returns the position of the item in the list
-    override fun getItemViewType(position: Int): Int {
-        return position
-    }
-
-    /**
-     * Handles the touch event for the CardView.
-     * Starts a drag event when the user touches down on the CardView.
-     */
-    @SuppressLint("ClickableViewAccessibility")
-    override fun onTouch(view: View, event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {
-            val data = ClipData.newPlainText("", "")
-            val shadowBuilder = DragShadowBuilder(view)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                view.startDragAndDrop(data, shadowBuilder, view, 0)
-            } else {
-                view.startDrag(data, shadowBuilder, view, 0)
-            }
-            return true
+        getItem(position)?.let { drawable ->
+            Glide.with(context).load(drawable).into(holder.ivGrid)
+            holder.cvGrid.tag = position
+            holder.cvGrid.setOnTouchListener(holder)
+            holder.cvGrid.setOnDragListener(DragListener(listener))
         }
-        return false
     }
 
-    /**
-     * Updates the list of drawables displayed in the RecyclerView.
-     */
-    fun updateList(list: List<Drawable>) {
-        this.list = list
-    }
+    class ListViewHolder(private val binding: ListItemMainBinding) : RecyclerView.ViewHolder(binding.root), View.OnTouchListener {
 
-    /**
-     * Returns the DragListener instance, which handles the drag events for the CardView.
-     */
-    val dragInstance: DragListener?
-        get() = if (listener != null) {
-            DragListener(listener)
-        } else {
-            null
-        }
-
-    /**
-     * ViewHolder for the items in the RecyclerView.
-     * Contains an ImageView for the drawable and a CardView for the item.
-     */
-    inner class ListViewHolder(binding: ListItemMainBinding) : ViewHolder(binding.root) {
-        var ivGrid: ImageView
-        var cvGrid: CardView
+        val ivGrid: ImageView = binding.ivGrid
+        val cvGrid: CardView = binding.cvGrid
 
         init {
-            ivGrid = binding.ivGrid
-            cvGrid = binding.cvGrid
+            itemView.setOnTouchListener(this)
         }
+
+        override fun onTouch(view: View, event: MotionEvent): Boolean {
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                val data = ClipData.newPlainText("", "")
+                val shadowBuilder = DragShadowBuilder(view)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    view.startDragAndDrop(data, shadowBuilder, view, 0)
+                } else {
+                    view.startDrag(data, shadowBuilder, view, 0)
+                }
+                return true
+            }
+            return false
+        }
+    }
+
+    class DiffCallback : DiffUtil.ItemCallback<Drawable>() {
+
+        override fun areItemsTheSame(oldItem: Drawable, newItem: Drawable): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Drawable, newItem: Drawable): Boolean {
+            return oldItem.constantState == newItem.constantState
+        }
+    }
+
+    fun updateList(list: List<Drawable>) {
+        submitList(list)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    class DragListener(private val listener: Listener?) : View.OnDragListener {
+
+        override fun onDrag(view: View, event: DragEvent): Boolean {
+            when (event.action) {
+                DragEvent.ACTION_DRAG_STARTED -> return true
+                DragEvent.ACTION_DRAG_ENTERED -> view.alpha = 0.5f
+                DragEvent.ACTION_DRAG_EXITED -> view.alpha = 1.0f
+                DragEvent.ACTION_DROP -> {
+                    val item = event.clipData.getItemAt(0).text.toString().toInt()
+                    listener?.onItemDropped(item, view)
+                    view.alpha = 1.0f
+                    return true
+                }
+                DragEvent.ACTION_DRAG_ENDED -> {
+                    view.alpha = 1.0f
+                    return true
+                }
+                else -> return false
+            }
+        }
+    }
+
+    interface Listener {
+        fun onItemDropped(item: Int, view: View)
     }
 }
