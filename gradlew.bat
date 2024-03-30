c2b6c835e0f92dd3ad0df39875330fafba0cf99e
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:22 -0700

    improve

--- gradlew.bat
+++ gradlew.bat
@@ -1,5 +1,17 @@
 @rem ****************************************************************************
 @rem 
 @rem  Gradle startup script for Windows
+@rem
+@rem This script sets the necessary environment variables for running Gradle
+@rem and adds Gradle distribution to the PATH.
 @rem 
-@rem ************************************************
+@rem ****************************************************************************
+
+@rem Set Gradle user home
+set "GRADLE_USER_HOME=%USERPROFILE%\.gradle"
+
+@rem Add Gradle distribution to PATH
+set "PATH=%GRADLE_USER_HOME%\gradle\gradle-6.8.3\bin;%PATH%"
+
+@rem Verify that Gradle is installed correctly
+gradle -v
