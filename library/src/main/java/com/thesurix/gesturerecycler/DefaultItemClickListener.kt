e395ea1bf84a5819993429a0feeab202375989b0
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:25 -0700

    comment

--- library/src/main/java/com/thesurix/gesturerecycler/DefaultItemClickListener.kt
+++ library/src/main/java/com/thesurix/gesturerecycler/DefaultItemClickListener.kt
@@ -1,19 +1,43 @@
 package com.thesurix.gesturerecycler
 
 /**
- * Default implementation of the [RecyclerItemTouchListener.ItemClickListener].
+ * This class provides a default implementation of the [RecyclerItemTouchListener.ItemClickListener] interface.
+ * It can be used to handle click and long press events for items in a RecyclerView.
+ *
  * @author thesurix
+ * @param <T> The type of data being displayed in the RecyclerView.
  */
 open class DefaultItemClickListener<T> : RecyclerItemTouchListener.ItemClickListener<T> {
 
+    /**
+     * This method is called when an item in the RecyclerView is clicked.
+     * By default, this method returns false, indicating that the click was not handled.
+     * Override this method to implement custom behavior when an item is clicked.
+     *
+     * @param item The data associated with the clicked item.
+     * @param position The position of the clicked item in the RecyclerView.
+     * @return True if the click was handled, false otherwise.
+     */
     override fun onItemClick(item: T, position: Int): Boolean {
         return false
     }
 
+    /**
+     * This method is called when an item in the RecyclerView is long pressed.
+     * By default, this method does nothing.
+     * Override this method to implement custom behavior when an item is long pressed.
+     *
+     * @param item The data associated with the long pressed item.
+     * @param position The position of the long pressed item in the RecyclerView.
+     */
     override fun onItemLongPress(item: T, position: Int) {
     }
 
-    override fun onDoubleTap(item: T, position: Int): Boolean {
-        return false
-    }
-}
+    /**
+     * This method is called when an item in the RecyclerView is double tapped.
+     * By default, this method returns false, indicating that the double tap was not handled.
+     * Override this method to implement custom behavior when an item is double tapped.
+     *
+     * @param item The data associated with the double tapped item.
+     * @param position The position of the double tapped item in the RecyclerView.
+     *
