ea3826aa5ab0087eb1f063edc44619aec877acaa
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:58 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/util/CustomDrawable.kt
+++ app/src/main/java/com/codesteem/mylauncher/util/CustomDrawable.kt
@@ -1,15 +1,5 @@
 package com.codesteem.mylauncher.util
 
+import android.content.res.Resources
 import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.drawable.Drawable
-
-/**
- * CustomDrawable class extends the Drawable class to create a custom drawable object.
- * This class provides a basic implementation of a drawable that draws a circle with the given paint settings.
- */
-class CustomDrawable : Drawable() {
-
-    /**
 
