dff9347a1ecc2fe992669ac16e02384531eabb12
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:44 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/data/remote/models/Choice.kt
+++ app/src/main/java/com/codesteem/mylauncher/data/remote/models/Choice.kt
@@ -1,8 +1,15 @@
-package com.codesteem.mylauncher.data.remote.models
-
+// Data class representing a single choice in a list of possible responses.
 data class Choice(
+
+    // The amount of time the user has to respond, represented as a Delta object.
     val delta: Delta,
+
+    // The reason why this choice was the last one presented to the user, or null if it wasn't.
     val finish_reason: String?,
+
+    // The index of this choice in the list of possible responses.
     val index: Int,
+
+    // The message associated with this choice, represented as a Message object.
     val message: Message
 )
