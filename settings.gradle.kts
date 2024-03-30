a3be8c5c951fe644369780b1e610a74c45b299b1
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:21 -0700

    comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,36 @@
+// Define the plugin management repositories
 pluginManagement {
     repositories {
+        // Include Google's Maven repository
         google()
+        // Include Maven Central repository
         mavenCentral()
+        // Include Gradle Plugin Portal repository
         gradlePluginPortal()
     }
 }
+
+// Configure dependency resolution management
 dependencyResolutionManagement {
+    // Fail on project repositories when repositories mode is set to 'FAIL_ON_PROJECT_REPOS'
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // Include Google's Maven repository
         google()
+        // Include Maven Central repository
         mavenCentral()
+        // Include Jitpack Maven repository
         maven { setUrl("https://jitpack.io") }
+        // Include JCenter repository
         jcenter()
     }
 }
 
+// Set the root project name to "Beta Backers"
 rootProject.name = "Beta Backers"
+
+// Include the 'app' module
 include(":app")
+
+// Commented out: include the 'library' module
 //include(":library")
