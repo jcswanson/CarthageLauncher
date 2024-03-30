4662d77db4da557b3c81684c502cf686f2c1e066
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:31 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/database/RuntimeExceptionAdapter.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/RuntimeExceptionAdapter.kt
@@ -1,30 +1,9 @@
-package com.codesteem.mylauncher.database
+/**
+ * A custom JSON serializer/deserializer for [RuntimeException] instances.
+ * This adapter is used to serialize and deserialize RuntimeException objects
+ * when converting data to and from JSON format using the Gson library.
+ */
+class RuntimeExceptionAdapter : JsonSerializer<RuntimeException>, JsonDeserializer<RuntimeException> {
 
-import com.google.gson.JsonDeserializationContext
-import com.google.gson.JsonDeserializer
-import com.google.gson.JsonElement
-import com.google.gson.JsonSerializationContext
-import com.google.gson.JsonSerializer
-import java.lang.reflect.Type
+    /**
 
-class RuntimeExceptionAdapter : JsonSerializer<RuntimeException>,
-    JsonDeserializer<RuntimeException> {
-
-    override fun serialize(
-        src: RuntimeException?,
-        typeOfSrc: Type?,
-        context: JsonSerializationContext?
-    ): JsonElement {
-        return context?.serialize(src)
-            ?: throw IllegalArgumentException("Unable to serialize RuntimeException without JsonSerializationContext")
-    }
-
-    override fun deserialize(
-        json: JsonElement?,
-        typeOfT: Type?,
-        context: JsonDeserializationContext?
-    ): RuntimeException {
-        return context?.deserialize(json, typeOfT)
-            ?: throw IllegalArgumentException("Unable to deserialize RuntimeException without JsonDeserializationContext")
-    }
-}
