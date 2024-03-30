0d5e15c1ea2f763c08decee887f3da1cdec4160d
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:50 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/database/NotificationEntity.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/NotificationEntity.kt
@@ -6,16 +6,33 @@ import androidx.room.TypeConverters
 import com.codesteem.mylauncher.database.converters.StringListConverter
 import com.codesteem.mylauncher.models.AppInfo
 
-@Entity(tableName = "notification_table")
-@TypeConverters(StringListConverter::class)
+// NotificationEntity: A data class representing a notification item in the local database.
+@Entity(tableName = "notification_table") // Entity annotation indicates that this class is a table in the database.
+@TypeConverters(StringListConverter::class) // TypeConverters allow Room to convert a List<String> to a single value in the database.
 data class NotificationEntity(
+
+    // id: A unique identifier for the notification.
     @PrimaryKey
     val id: String,
+
+    // title: The title of the notification.
     val title: String?,
+
+    // text: The text content of the notification.
     val text: String?,
+
+    // timeStamp: The time the notification was received.
     val timeStamp: Long?,
+
+    // appInfo: Additional information about the app associated with the notification.
     val appInfo: AppInfo?,
+
+    // actions: A list of actions associated with the notification.
     val actions: List<String>,
+
+    // isPrioritized: A flag indicating whether the notification is prioritized.
     var isPrioritized: Boolean = false,
+
+    // isExpanded: A flag indicating whether the notification is expanded.
     var isExpanded: Boolean = false
 )
