be023db0b0322807f8141278109819b8a5199bcc
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:51 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/util/Constants.kt
+++ app/src/main/java/com/codesteem/mylauncher/util/Constants.kt
@@ -11,11 +11,23 @@ object Constants {
      * The base URL for the Perplexity API. This URL is used as the starting point for all API
      * requests made to the Perplexity API.
      */
-    const val BASE_URL = "https://api.perplexity.ai/"
+    @Suppress("SpellCheckingInspection")
+    const val BASE_URL: String = "https://api.perplexity.ai/"
 
     /**
      * A string constant representing the category for bugs. This constant may be used to
      * categorize or filter items related to bugs or issues in the application.
      */
-    const val BUGS = "Bugs"
-}
+    const val BUGS: String = "Bugs"
+
+    /**
+     * A string constant representing the version of the Perplexity API. This constant may be used to
+     * track and manage changes to the API.
+     */
+    const val VERSION: String = "v1"
+
+    /**
+     * A string constant representing the user agent of the client application. This constant may be used to
+     * identify and log the client application in the server.
+     */
+    const val USER_AGENT: String = "MyLauncher/${BuildConfig.VERSION_NAME
