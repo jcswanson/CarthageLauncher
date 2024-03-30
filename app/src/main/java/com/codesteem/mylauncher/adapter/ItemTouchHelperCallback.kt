6c06f04b033572e6086fd54bcafb388bf85f1c87
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:52 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/adapter/ItemTouchHelperCallback.kt
+++ app/src/main/java/com/codesteem/mylauncher/adapter/ItemTouchHelperCallback.kt
@@ -5,21 +5,38 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.codesteem.mylauncher.test.MonthsAdapter
 
+/**
+ * A custom [ItemTouchHelper.Callback] implementation for the [MonthsAdapter] to handle drag and swipe gestures.
+ *
+ * @property adapter The [MonthsAdapter] instance to interact with.
+ */
 class ItemTouchHelperCallback(
     private val adapter: MonthsAdapter
 ) : ItemTouchHelper.Callback() {
 
+    /**
+     * Returns the movement flags for the given [recyclerView] and [viewHolder]. In this case,
+     * allowing both dragging and swiping in all directions.
+     */
     override fun getMovementFlags(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder
     ): Int {
-        // Allow dragging and swiping in all directions
         return ItemTouchHelper.Callback.makeMovementFlags(
-            ItemTouchHelper.ACTION_STATE_DRAG,
-            ItemTouchHelper.ACTION_STATE_SWIPE
+            ItemTouchHelper.ACTION_STATE_DRAG, // Allowing dragging
+            ItemTouchHelper.ACTION_STATE_SWIPE // Allowing swiping
         )
     }
 
+    /**
+     * Called when an item is moved in the [recyclerView]. This method updates the adapter's data and
+     * notifies the adapter of the change.
+     *
+     * @param recyclerView The [RecyclerView] instance.
+     * @param viewHolder   The [RecyclerView.ViewHolder] instance representing the source item.
+     * @param target       The [RecyclerView.ViewHolder] instance representing the target item.
+     * @return True if the move is successful, false otherwise.
+     */
     override fun onMove(
         recyclerView: RecyclerView,
         viewHolder: RecyclerView.ViewHolder,
@@ -27,11 +44,4 @@ class ItemTouchHelperCallback(
     ): Boolean {
         Log.e("onMove", "onMove")
         adapter.move(viewHolder.adapterPosition, target.adapterPosition)
-        adapter.notifyItemMoved(viewHolder.adapterPosition, target.adapterPosition)
-        return true
-    }
 
-    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-        Log.e("onSwipe", "onSwipe")
-    }
-}
