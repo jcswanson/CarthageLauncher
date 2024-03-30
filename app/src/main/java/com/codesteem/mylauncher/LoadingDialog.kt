0bd337d210a6c38cd4f70b7836f58a6f3cb8fb84
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:41 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/LoadingDialog.kt
+++ app/src/main/java/com/codesteem/mylauncher/LoadingDialog.kt
@@ -8,34 +8,46 @@ import android.os.Bundle
 import android.view.Window
 import com.codesteem.mylauncher.databinding.LoadingDialogBinding
 
+/**
+ * A custom loading dialog class for displaying a loading animation with an optional title.
+ * Users can set an OnActionListener to handle the OK and Cancel button clicks.
+ */
 class LoadingDialog(
-    context: Context,
-    private val title: String?
-): Dialog(context) {
-    private lateinit var binding: LoadingDialogBinding
-    private var mListener: OnActionListener? = null
+    context: Context, // Context of the calling component
+    private val title: String? // Optional title for the dialog
+) : Dialog(context) {
+    private lateinit var binding: LoadingDialogBinding // View binding for the dialog layout
+    private var mListener: OnActionListener? = null // Listener for button clicks
 
+    /**
+     * Dialog creation method, inflates the layout, sets the content view, and configures the dialog window.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LoadingDialogBinding.inflate(layoutInflater)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        requestWindowFeature(Window.FEATURE_NO_TITLE) // Removes the default dialog title
         setContentView(binding.root)
+
+        // Calculates the width of the dialog to be 90% of the screen width
         val width = context.resources.displayMetrics.widthPixels * 0.90
         binding.root.layoutParams.width = width.toInt()
+
+        // Sets the background of the dialog window to be transparent
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
+        // Sets the title if provided
         if (title != null) {
             binding.title.text = title
         }
     }
 
+    /**
+     * Interface for handling button clicks in the dialog
+     */
     interface OnActionListener {
-        fun onOK()
-        fun onCancel()
+        fun onOK() // Method for handling the OK button click
+        fun onCancel() // Method for handling the Cancel button click
     }
 
-    fun setOnActionListener(listener: OnActionListener?) {
-        mListener = listener
-    }
+    /**
 
-}
