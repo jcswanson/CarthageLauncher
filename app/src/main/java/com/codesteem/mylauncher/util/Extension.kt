aa541db3c420c9c50aaed5d3db7e6886c018c781
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:56 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/util/Extension.kt
+++ app/src/main/java/com/codesteem/mylauncher/util/Extension.kt
@@ -1,24 +1,27 @@
-package com.codesteem.mylauncher.util
-
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
+import android.os.Build
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 
 /**
  * Copies the given text to the clipboard service of the system.
- * 
+ *
  * @param text The text to copy to the clipboard.
  * @param context The context of the application.
  */
-fun copyToClipboard(text: String, context: Context) {
+fun String.copyToClipboard(context: Context?) {
+    if (context == null) {
+        throw IllegalArgumentException("Context cannot be null")
+    }
+
     // Get the clipboard manager service from the system
     val clipboardManager = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
     // Create a new ClipData with a plain text MIME type and the text to copy
-    val clipData = ClipData.newPlainText("text label", text)
+    val clipData = ClipData.newPlainText("text label", this)
 
     // Set the ClipData to the clipboard
     clipboardManager.setPrimaryClip(clipData)
@@ -30,11 +33,14 @@ fun copyToClipboard(text: String, context: Context) {
 
 /**
  * Hides the keyboard associated with the given view.
- * 
+ *
  * @param view The view associated with the keyboard to hide.
  */
-fun hideKeyboard(view: View) {
+fun View.hideKeyboard() {
     // Get the input method manager service from the system
-    val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(view.windowToken, 0)
-}
+    val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+    // Check if the device is running Android 6.0 (API level 23) or higher
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        // Use the new hideSoftInputFromWindow() method with the FLAG_HIDE_SOFT_INPUT flag
+        imm.hideSoftInputFromWindow(this.window
