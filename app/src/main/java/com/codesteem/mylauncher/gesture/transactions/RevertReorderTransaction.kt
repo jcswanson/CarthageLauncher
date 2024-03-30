a700aebaf9c367bdf1133a646ce94fcd5690c0b1
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:18 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/gesture/transactions/RevertReorderTransaction.kt
+++ app/src/main/java/com/codesteem/mylauncher/gesture/transactions/RevertReorderTransaction.kt
@@ -1,22 +1,30 @@
 package com.codesteem.mylauncher.gesture.transactions
 
-
 /**
+ * A transaction to revert a reorder of items in a list.
+ *
  * @author thesurix
  */
-class RevertReorderTransaction<T>(private val from: Int,
-                                  private val to: Int,
-                                  private val headerEnabled: Boolean) : Transaction<T> {
+class RevertReorderTransaction<T>(
+    private val from: Int,
+    private val to: Int,
+    private val headerEnabled: Boolean,
+    private val transactional: Transactional<T>
+) : Transaction<T> {
 
-    override fun perform(transactional: Transactional<T>) = false
+    override fun perform(transactional: Transactional<T>): Boolean {
+        return revert(transactional)
+    }
 
     override fun revert(transactional: Transactional<T>): Boolean {
         return with(transactional.data) {
             val item = removeAt(to)
             item?.let {
-                transactional.notifyRemoved(to + if (headerEnabled) 1 else 0)
-                add(from, it)
-                transactional.notifyInserted(from + if (headerEnabled) 1 else 0)
+                val adjustedFrom = from + if (headerEnabled) 1 else 0
+                val adjustedTo = to + if (headerEnabled) 1 else 0
+                transactional.notifyRemoved(adjustedTo)
+                add(adjustedFrom, it)
+                transactional.notifyInserted(adjustedFrom)
                 true
             } ?: false
         }
