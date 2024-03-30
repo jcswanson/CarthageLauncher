5ebf57ae7056b99facbed355b39703ade5e2367d
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:08 -0700

    improve

--- library/src/main/java/com/thesurix/gesturerecycler/DefaultItemClickListener.kt
+++ library/src/main/java/com/thesurix/gesturerecycler/DefaultItemClickListener.kt
@@ -11,15 +11,14 @@ open class DefaultItemClickListener<T> : RecyclerItemTouchListener.ItemClickList
 
     /**
      * This method is called when an item in the RecyclerView is clicked.
-     * By default, this method returns false, indicating that the click was not handled.
+     * By default, this method does nothing.
      * Override this method to implement custom behavior when an item is clicked.
      *
      * @param item The data associated with the clicked item.
      * @param position The position of the clicked item in the RecyclerView.
-     * @return True if the click was handled, false otherwise.
      */
-    override fun onItemClick(item: T, position: Int): Boolean {
-        return false
+    override fun onItemClick(item: T, position: Int) {
+        // Do nothing by default
     }
 
     /**
@@ -31,6 +30,7 @@ open class DefaultItemClickListener<T> : RecyclerItemTouchListener.ItemClickList
      * @param position The position of the long pressed item in the RecyclerView.
      */
     override fun onItemLongPress(item: T, position: Int) {
+        // Do nothing by default
     }
 
     /**
@@ -39,5 +39,4 @@ open class DefaultItemClickListener<T> : RecyclerItemTouchListener.ItemClickList
      * Override this method to implement custom behavior when an item is double tapped.
      *
      * @param item The data associated with the double tapped item.
-     * @param position The position of the double tapped item in the RecyclerView.
-     *
+    
