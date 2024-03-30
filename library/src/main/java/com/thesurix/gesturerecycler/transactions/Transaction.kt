8203c4a608cae410e621c493cf782e6c2d10a521
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:14 -0700

    comment

--- library/src/main/java/com/thesurix/gesturerecycler/transactions/Transaction.kt
+++ library/src/main/java/com/thesurix/gesturerecycler/transactions/Transaction.kt
@@ -1,9 +1,28 @@
 package com.thesurix.gesturerecycler.transactions
 
 /**
+ * Interface for a transaction that can be performed and reverted on a [Transactional] object.
+ * A transaction represents a single unit of work that can be executed and then undone,
+ * leaving the system in the transactional object in the same state as before the transaction.
+ *
  * @author thesurix
  */
 interface Transaction<T> {
+    /**
+     * Performs the transaction on the given [transactional] object.
+     * Returns true if the transaction was performed successfully, false otherwise.
+     *
+     * @param transactional The object on which the transaction will be performed.
+     * @return True if the transaction was performed successfully, false otherwise.
+     */
     fun perform(transactional: Transactional<T>): Boolean
+
+    /**
+     * Reverts the transaction on the given [transactional] object.
+     * Returns true if the transaction was reverted successfully, false otherwise.
+     *
+     * @param transactional The object on which the transaction will be reverted.
+     * @return True if the transaction was reverted successfully, false otherwise.
+     */
     fun revert(transactional: Transactional<T>): Boolean
 }
