26c4f7254aa5d281a636f16cd0b51e9461746c80
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:18:44 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/models/AppSearchModel.kt
+++ app/src/main/java/com/codesteem/mylauncher/models/AppSearchModel.kt
@@ -4,8 +4,18 @@ import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 
 data class AppSearchModel(
-    val title: String?="",
-    val userImage: Drawable,
-    val packageName: String?="",
+    // The name of the app or activity
+    val title: String? = "",
 
+    // The user-facing image for the app or activity
+    val userImage: Drawable,
+
+    // The package name of the app or activity
+    val packageName: String? = ""
 )
+
+// This data class represents a model for an app or activity search result
+// It contains three properties:
+// - title: The name of the app or activity
+// - userImage: The user-facing image for the app or activity
+// - packageName: The package name of the app or activity
