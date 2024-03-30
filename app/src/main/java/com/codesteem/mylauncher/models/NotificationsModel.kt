ae14ed02094ff87de12318bed63e6a08c583dc91
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:26 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/models/NotificationsModel.kt
+++ app/src/main/java/com/codesteem/mylauncher/models/NotificationsModel.kt
@@ -2,17 +2,63 @@ package com.codesteem.mylauncher.models
 
 import android.graphics.Bitmap
 
+/**
+ * Data class representing a notification model with various properties.
+ */
 data class NotificationsModel(
-    val time: String?="",
-    val userImage: String?="",
-    val title: String?="",
-    val desc: String?="",
-    val mediaImage: Bitmap?=null,
-    val notificationType: String?="",
-    val alarmTime: String?="",
-    val relatedAppPackageName: String? = "",
-    val app_name: String? = "",
-    val app_icon: String?=null,
-    val notiKey: String?=null,
 
-    )
+    /**
+     * The time at which the notification was received.
+     */
+    val time: String? = "",
+
+    /**
+     * The URL or file path of the user's image.
+     */
+    val userImage: String? = "",
+
+    /**
+     * The title of the notification.
+     */
+    val title: String? = "",
+
+    /**
+     * The description or body of the notification.
+     */
+    val desc: String? = "",
+
+    /**
+     * The media image associated with the notification.
+     */
+    val mediaImage: Bitmap? = null,
+
+    /**
+     * The type of notification.
+     */
+    val notificationType: String? = "",
+
+    /**
+     * The alarm time associated with the notification.
+     */
+    val alarmTime: String? = "",
+
+    /**
+     * The package name of the related app.
+     */
+    val relatedAppPackageName: String? = "",
+
+    /**
+     * The name of the related app.
+     */
+    val app_name: String? = "",
+
+    /**
+     * The file path or URL of the app icon.
+     */
+    val app_icon: String? = null,
+
+    /**
+     * A unique key for the notification.
+     */
+    val notiKey: String? = null
+)
