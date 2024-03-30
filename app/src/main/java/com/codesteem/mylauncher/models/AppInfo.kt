2651c7a6926cf3cdb599482abd05179ba30c5734
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:02 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/models/AppInfo.kt
+++ app/src/main/java/com/codesteem/mylauncher/models/AppInfo.kt
@@ -4,28 +4,15 @@ import android.graphics.drawable.Drawable
 import android.os.Parcel
 import android.os.Parcelable
 
-data class AppInfo(val appName: String, val appIconByteArray: ByteArray) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.createByteArray()!!
-    )
+/**
+ * Data class representing information about an app, including its name and icon.
+ * The icon is stored as a byte array, allowing for flexibility in its usage.
+ * Implements the Parcelable interface for efficient data transfer between app components.
+ */
+data class AppInfo(
+    val appName: String, // Name of the app
+    val appIconByteArray: ByteArray // Icon of the app, stored as a byte array
+) : Parcelable {
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(appName)
-        parcel.writeByteArray(appIconByteArray)
-    }
+    /**
 
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<AppInfo> {
-        override fun createFromParcel(parcel: Parcel): AppInfo {
-            return AppInfo(parcel)
-        }
-
-        override fun newArray(size: Int): Array<AppInfo?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
