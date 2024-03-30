f62fafe50a3f8170bee749ee8df11e64d8a7c224
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:29 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/anothertest/Listener.kt
+++ app/src/main/java/com/codesteem/mylauncher/anothertest/Listener.kt
@@ -1,27 +1,13 @@
 /**
- * This interface defines a contract for a listener that can be used to show or hide various
- * empty list views in the UI.
+ * This interface defines a contract for a listener that can be used to show or hide empty list views.
  */
-interface Listener {
+interface EmptyListListener {
 
     /**
-     * Sets the visibility of the empty list top view.
+     * Sets the visibility of the empty list view.
      *
-     * @param visibility `true` to show the empty list top view, `false` to hide it.
+     * @param viewName The name of the empty list view to show or hide.
+     * @param visibility `true` to show the empty list view, `false` to hide it.
      */
-    fun setEmptyListTop(visibility: Boolean)
-
-    /**
-     * Sets the visibility of the empty list bottom view.
-     *
-     * @param visibility `true` to show the empty list bottom view, `false` to hide it.
-     */
-    fun setEmptyListBottom(visibility: Boolean)
-
-    /**
-     * Sets the visibility of the empty list middle view.
-     *
-     * @param visibility `true` to show the empty list middle view, `false` to hide it.
-     */
-    fun setEmptyListMid(visibility: Boolean)
+    fun setEmptyListView(viewName: String, visibility: Boolean)
 }
