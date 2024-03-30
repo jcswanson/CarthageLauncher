362fcd6536752627d7a771ac878e02bcccaa059d
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:49:12 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/test/GridItemViewHolder.kt
+++ app/src/main/java/com/codesteem/mylauncher/test/GridItemViewHolder.kt
@@ -6,27 +6,20 @@ import android.widget.TextView
 import com.codesteem.mylauncher.databinding.GridItemBinding
 
 // Custom ViewHolder for the grid items in the calendar view
-class GridItemViewHolder (
-    private val binding: GridItemBinding // GridItemBinding instance for data binding
-) : BaseMonthViewHolder(binding.root) {
+class GridItemViewHolder(private val binding: GridItemBinding) : BaseMonthViewHolder(binding.root) {
 
-    // Overriding the 'monthText' property from the BaseMonthViewHolder
     override val monthText: TextView
-        get() = binding.monthText // Returns the TextView for displaying the month name
+        get() = binding.monthText
 
-    // Overriding the 'monthPicture' property from the BaseMonthViewHolder
     override val monthPicture: ImageView
-        get() = binding.monthImage // Returns the ImageView for displaying the month picture
+        get() = binding.monthImage
 
-    // Overriding the 'itemDrag' property from the BaseMonthViewHolder
     override val itemDrag: ImageView
-        get() = binding.monthDrag // Returns the ImageView for handling the drag event
+        get() = binding.monthDrag
 
-    // Overriding the 'tvCounter' property from the BaseMonthViewHolder
     override val tvCounter: TextView
-        get() = binding.tvCounter // Returns the TextView for displaying the counter
+        get() = binding.tvCounter
 
-    // Overriding the 'foreground' property from the BaseMonthViewHolder
     override val foreground: View?
-        get() = null // Returns null as there's no specific foreground view for this ViewHolder
+        get() = binding.root // Returns the root view as the foreground view
 }
