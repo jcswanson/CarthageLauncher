e63df71b4b5323588b8ecb4c71b73c6977599022
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:19:28 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/gesture/transactions/Transactional.kt
+++ app/src/main/java/com/codesteem/mylauncher/gesture/transactions/Transactional.kt
@@ -1,12 +1,47 @@
 package com.codesteem.mylauncher.gesture.transactions
 
 /**
+ * Interface for objects that can perform transactional operations on a list of data items.
+ *
  * @author thesurix
  */
 interface Transactional<T> {
+    /**
+     * The list of data items that this transactional object manages.
+     * This list is mutable, meaning that it can be modified directly.
+     */
     val data: MutableList<T>
+
+    /**
+     * Notifies listeners that the data item at the specified position has changed.
+     * This method should be called when the data item at the given position has been modified.
+     *
+     * @param position The position of the data item that has changed.
+     */
     fun notifyChanged(position: Int)
+
+    /**
+     * Notifies listeners that a new data item has been inserted at the specified position.
+     * This method should be called when a new data item has been added to the list.
+     *
+     * @param position The position where the new data item has been inserted.
+     */
     fun notifyInserted(position: Int)
+
+    /**
+     * Notifies listeners that the data item at the specified position has been removed.
+     * This method should be called when a data item has been removed from the list.
+     *
+     * @param position The position of the data item that has been removed.
+     */
     fun notifyRemoved(position: Int)
+
+    /**
+     * Notifies listeners that the data item at the specified position has been moved to a new position.
+     * This method should be called when a data item has been moved within the list.
+     *
+     * @param fromPosition The original position of the data item.
+     * @param toPosition The new position of the data item.
+     */
     fun notifyMoved(fromPosition: Int, toPosition: Int)
 }
