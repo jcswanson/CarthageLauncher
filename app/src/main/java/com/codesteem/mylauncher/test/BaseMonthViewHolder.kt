936231efea154907fdc06ba68f9309a55e2e0ba6
John Christian Swanson <johncswan@hotmail.com>
2024-03-29 18:48:58 -0700

    improve

--- app/src/main/java/com/codesteem/mylauncher/test/BaseMonthViewHolder.kt
+++ app/src/main/java/com/codesteem/mylauncher/test/BaseMonthViewHolder.kt
@@ -1,13 +1,8 @@
-// BaseMonthViewHolder is an abstract class that extends GestureViewHolder and provides basic functionality for displaying and managing MonthItem objects.
-package com.codesteem.mylauncher.test
-
-// Import necessary Android libraries and custom classes.
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
-import android.util.Log
+import android.content.Context
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.animation.LinearInterpolator
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -15,79 +10,53 @@ import com.codesteem.mylauncher.R
 import com.codesteem.mylauncher.adapter.DragListener
 import com.codesteem.mylauncher.gesture.GestureViewHolder
 
-// Define the BaseMonthViewHolder class with a constructor that takes a rootView parameter.
 abstract class BaseMonthViewHolder(
-    private val rootView: View
+    private val rootView: View,
+    protected val tvCounter: TextView,
+    protected val monthText: TextView,
+    protected val monthPicture: ImageView,
+    protected val itemDrag: ImageView,
+    protected val foreground: View?
 ) : GestureViewHolder<MonthItem>(rootView) {
 
-    // Declare abstract properties that must be implemented by the subclasses.
-    protected abstract val tvCounter: TextView
-    protected abstract val monthText: TextView
-    protected abstract val monthPicture: ImageView
-    protected abstract val itemDrag: ImageView
-    protected abstract val foreground: View?
-
-    // Override the draggableView property to return the itemDrag ImageView.
     override val draggableView: View?
         get() = itemDrag
 
-    // Override the foregroundView property to return the foreground View or the superclass's foregroundView if it's null.
     override val foregroundView: View
         get() = foreground ?: super.foregroundView
 
-    // Implement the bind() method to set the properties of the Views based on the provided MonthItem.
     override fun bind(item: MonthItem) {
-        if (item is Month) { // Check if the item is an instance of Month.
-            monthText.text = item.name // Set the monthText's text to the item's name.
-            if (item.counterNotification!=null&&item.counterNotification!=0){ // If the item has a non-null and non-zero counterNotification.
-                tvCounter.visibility=View.VISIBLE // Make the counter visible.
-                tvCounter.text = (item.counterNotification).toString() // Set the counter's text to the counterNotification value.
-            }else{
-                tvCounter.visibility=View.GONE // Otherwise, hide the counter.
-
-            }
-            Glide.with(itemView.context) // Use Glide to load the item's drawable into the monthPicture ImageView.
-                    .load(item.drawableId)
-                    .apply(RequestOptions.centerCropTransform())
-                    .into(monthPicture)
+        if (item is Month) {
+            monthText.text = item.name
+            tvCounter.visibility = if (item.counterNotification != null && item.counterNotification != 0) View.VISIBLE else View.GONE
+            tvCounter.text = item.counterNotification.toString()
+            Glide.with(itemView.context)
+                .load(item.drawableId)
+                .apply(RequestOptions.centerCropTransform())
+                .into(monthPicture)
         }
     }
 
-    // Implement the onItemSelect() method to change the appearance of the Views when the item is selected.
     override fun onItemSelect() {
-        rootView.setOnDragListener(DragListener()) // Set the rootView's OnDragListener to the DragListener.
-        val textColorFrom = ContextCompat.getColor(itemView.context, android.R.color.white) // Define the starting text color.
-        val textColorTo = ContextCompat.getColor(itemView.context, R.color.black) // Define the ending text color.
-        ValueAnimator.ofObject(ArgbEvaluator(), textColorFrom, textColorTo).apply { // Create a ValueAnimator that changes the text color.
-            duration = itemView.context.resources.getInteger(R.integer.animation_speed_ms).toLong() // Set the duration of the animation.
-            addUpdateListener(getTextAnimatorListener(monthText, this)) // Add a listener to update the text color when the animation updates.
-            start() // Start the animation.
-        }
-
-
-        val backgroundColorFrom = ContextCompat.getColor(itemView.context, R.color.black) // Define the starting background color.
-        val backgroundColorTo = ContextCompat.getColor(itemView.context, android.R.color.white) // Define the ending background color.
-        ValueAnimator.ofObject(ArgbEvaluator(), backgroundColorFrom, backgroundColorTo).apply { // Create a ValueAnimator that changes the background color.
-            duration = itemView.context.resources.getInteger(R.integer.animation_speed_ms).toLong() // Set the duration of the animation.
-            addUpdateListener(getBackgroundAnimatorListener(monthText, this)) // Add a listener to update the background color when the animation updates.
-            start() // Start the animation.
-        }
+        rootView.setOnDragListener(DragListener())
+        animateTextAndBackgroundColor(monthText, R.color.white, R.color.black)
     }
 
-    // Implement the onItemClear() method to change the appearance of the Views when the item is cleared.
     override fun onItemClear() {
-        val textColorFrom = ContextCompat.getColor(itemView.context, R.color.black) // Define the starting text color.
-        val textColorTo = ContextCompat.getColor(itemView.context, android.R.color.white) // Define the ending text color.
-        ValueAnimator.ofObject(ArgbEvaluator(), textColorFrom, textColorTo).apply { // Create a ValueAnimator that changes the text color.
-            duration = itemView.context.resources.getInteger(R.integer.animation_speed_ms).toLong() // Set the duration of the animation.
-            addUpdateListener(getTextAnimatorListener(monthText, this)) // Add a listener to update the text color when the animation updates.
-            start() // Start the animation.
-        }
+        animateTextAndBackgroundColor(monthText, R.color.black, R.color.white)
+    }
 
+    private fun animateTextAndBackgroundColor(view: View, startColor: Int, endColor: Int) {
+        val textColorAnimator = ValueAnimator.ofObject(ArgbEvaluator(), ContextCompat.getColor(itemView.context, startColor), ContextCompat.getColor(itemView.context, endColor))
+        textColorAnimator.interpolator = LinearInterpolator()
+        textColorAnimator.duration = itemView.context.resources.getInteger(R.integer.animation_speed_ms).toLong()
+        textColorAnimator.addUpdateListener { animator -> view.setBackgroundColor(animator.animatedValue as Int) }
+        textColorAnimator.start()
 
-        val backgroundColorFrom = ContextCompat.getColor(itemView.context, android.R.color.white) // Define the starting background color.
-        val backgroundColorTo = ContextCompat.getColor(itemView.context, R.color.black) // Define the ending background color.
-        ValueAnimator.ofObject(ArgbEvaluator(), backgroundColorFrom, backgroundColorTo).apply { // Create a ValueAnimator that changes the background color.
-            duration = itemView.context.resources.getInteger(R.integer.animation_speed_ms).toLong() // Set the duration of the animation.
-            addUpdateListener(getBackgroundAnimatorListener(monthText, this)) // Add a listener to update the background color when the animation updates.
-            start()
+        val backgroundColorAnimator = ValueAnimator.ofObject(ArgbEvaluator(), ContextCompat.getColor(itemView.context, endColor), ContextCompat.getColor(itemView.context, startColor))
+        backgroundColorAnimator.interpolator = LinearInterpolator()
+        backgroundColorAnimator.duration = itemView.context.resources.getInteger(R.integer.animation_speed_ms).toLong()
+        backgroundColorAnimator.addUpdateListener { animator -> view.setBackgroundColor(animator.animatedValue as Int) }
+        backgroundColorAnimator.start()
+    }
+}
