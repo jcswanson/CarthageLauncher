52eaaac17638fc69b003b4e96a0916067ec2daa5
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:35 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/database/converters/PendingIntentInfo.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/converters/PendingIntentInfo.kt
@@ -1,10 +1,16 @@
-package com.codesteem.mylauncher.database.converters
-
-import android.content.Intent
-
+// The `PendingIntentInfo` data class holds information about a PendingIntent, which is a token for starting an operation
+// in the future. This class is used in the database layer of the MyLauncher app.
 data class PendingIntentInfo(
+
+    // The `requestCode` is a unique integer that identifies this request. It's used to ensure that the correct
+    // PendingIntent is delivered to the target application.
     val requestCode: Int,
+
+    // The `intent` is the Intent that describes the operation to perform when the PendingIntent is triggered.
     val intent: Intent,
+
+    // The `flags` are a set of flags that modify the behavior of the PendingIntent. For example, the `FLAG_UPDATE_CURRENT`
+    // flag can be used to update the existing PendingIntent with the new information.
     val flags: Int
     // Add other necessary information
 )
