4a8dcc047ca495107d86a6a22140986d720d48e5
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:10:02 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/data/remote/models/Usage.kt
+++ app/src/main/java/com/codesteem/mylauncher/data/remote/models/Usage.kt
@@ -1,7 +1,12 @@
-package com.codesteem.mylauncher.data.remote.models
-
+// Data class representing usage information for a model.
 data class Usage(
+
+    // The number of completion tokens used.
     val completion_tokens: Int,
+
+    // The number of prompt tokens used.
     val prompt_tokens: Int,
+
+    // The total number of tokens used.
     val total_tokens: Int
 )
