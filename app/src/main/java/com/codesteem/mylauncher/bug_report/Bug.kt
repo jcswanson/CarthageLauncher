9e82daf5376a7eaf737ead27bb17c2601ec45be1
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:33 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/bug_report/Bug.kt
+++ app/src/main/java/com/codesteem/mylauncher/bug_report/Bug.kt
@@ -1,8 +1,17 @@
 package com.codesteem.mylauncher.bug_report
 
+// `Bug` data class representing a single bug report
 data class Bug(
+    // Unique identifier for the bug report
     var id: String = "",
+
+    // Title or short summary of the bug
     var title: String = "",
+
+    // Detailed description of the bug, including steps to reproduce and expected vs. actual behavior
     var description: String = "",
+
+    // List of media file paths (screenshots, logs, etc.) related to the bug report
     var media: ArrayList<String> = arrayListOf("")
 )
+
