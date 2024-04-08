30f53189c99b92532cb87ea096454447b7a2e02a
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:58 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/data/remote/models/Message.kt
+++ app/src/main/java/com/codesteem/mylauncher/data/remote/models/Message.kt
@@ -1,6 +1,11 @@
-package com.codesteem.mylauncher.data.remote.models
-
+// A data class representing a message object with two properties:
+// - content: The actual message as a string.
+// - role: The role of the message, such as 'assistant' or 'user'.
 data class Message(
+
+    // The content of the message. This is a required property and should not be null.
     val content: String,
+
+    // The role of the message. This is a required property and should not be null.
     val role: String
 )
