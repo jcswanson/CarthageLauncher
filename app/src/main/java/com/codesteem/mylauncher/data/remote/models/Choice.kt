be4712c0b1f682884c3b6614a3f7d69b63712527
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:55 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/data/remote/models/Choice.kt
+++ app/src/main/java/com/codesteem/mylauncher/data/remote/models/Choice.kt
@@ -1,8 +1,15 @@
-package com.codesteem.mylauncher.data.remote.models
-
+// Data class representing a single choice in a list of possible responses.
 data class Choice(
+
+    // The amount of time the user has to respond, represented as a Delta object.
     val delta: Delta,
+
+    // The reason why the assistant finished generating this choice, if applicable.
     val finish_reason: String?,
+
+    // The index of this choice in the list of possible responses.
     val index: Int,
+
+    // The message associated with this choice, which may include text or other media.
     val message: Message
 )
