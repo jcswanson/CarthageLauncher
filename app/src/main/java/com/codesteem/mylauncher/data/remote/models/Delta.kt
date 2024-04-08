ab1666e29218be678968c3d6a39c760993bfa261
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:58 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/data/remote/models/Delta.kt
+++ app/src/main/java/com/codesteem/mylauncher/data/remote/models/Delta.kt
@@ -1,6 +1,9 @@
-package com.codesteem.mylauncher.data.remote.models
-
+// Data class representing a delta update, which contains new or updated content and its corresponding role.
 data class Delta(
+
+    // The updated or new content. This can be any data type or structure, but is typically a string.
     val content: String,
+
+    // The role of the content, indicating its purpose or category. This can be used to differentiate between different types of content in the update.
     val role: String
 )
