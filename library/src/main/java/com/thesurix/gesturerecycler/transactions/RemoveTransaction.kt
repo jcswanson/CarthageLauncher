7d104293c7665ee551026a4a7e582a4a314a81bf
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:48 -0700

    comment

--- library/src/main/java/com/thesurix/gesturerecycler/transactions/RemoveTransaction.kt
+++ library/src/main/java/com/thesurix/gesturerecycler/transactions/RemoveTransaction.kt
@@ -1,31 +1,48 @@
 package com.thesurix.gesturerecycler.transactions
 
-
 /**
+ * The RemoveTransaction class is responsible for performing and reverting the removal of an item
+ * from a list in a transactional manner. This class is generic, allowing it to work with any
+ * type of data (represented by the type parameter T).
+ *
  * @author thesurix
  */
-class RemoveTransaction<T>(private val position: Int,
-                           private val headerEnabled: Boolean) : Transaction<T> {
+class RemoveTransaction<T>(
+    private val position: Int, // The position of the item to be removed
+    private val headerEnabled: Boolean // A flag indicating whether the list has a header
+) : Transaction<T> {
+
+    // The removed item, kept for revert operation
     private var item: T? = null
 
+    /**
+     * Performs the removal of the item at the specified position from the given data list.
+     * If the item was successfully removed, the corresponding item removal is notified to
+     * the Transactional object.
+     *
+     * @param transactional The Transactional object containing the data list to be modified
+     * @return true if the item was removed, false otherwise
+     */
     override fun perform(transactional: Transactional<T>): Boolean {
         return with(transactional.data) {
-            val removedItem = removeAt(position)
+            val removedItem = removeAt(position) // Remove the item at the specified position
             removedItem?.let {
-                item = it
-                transactional.notifyRemoved(position + if(headerEnabled) 1 else 0)
+                item = it // Save the removed item for revert operation
+                transactional.notifyRemoved(position + if (headerEnabled) 1 else 0) // Notify the removal
                 true
             } ?: false
         }
     }
 
+    /**
+     * Reverts the removal of the item by adding it back to the data list at the same position.
+     * If the item was successfully added back, the corresponding item insertion is notified
+     * to the Transactional object.
+     *
+     * @param transactional The Transactional object containing the data list to be modified
+     * @return true if the item was added back, false otherwise
+     */
     override fun revert(transactional: Transactional<T>): Boolean {
         return with(transactional.data) {
             item?.let {
-                add(position, it)
-                transactional.notifyInserted(position + if(headerEnabled) 1 else 0)
-                true
-            } ?: false
-        }
-    }
-}
+               
