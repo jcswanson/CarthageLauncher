0123779ec07581814ae832d782cb889d3ba449ee
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:39 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/models/AppSearchModel.kt
+++ app/src/main/java/com/codesteem/mylauncher/models/AppSearchModel.kt
@@ -3,9 +3,19 @@ package com.codesteem.mylauncher.models
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 
+// AppSearchModel is a data class that represents a model for app search results
 data class AppSearchModel(
+
+    // The title of the app
     val title: String?="",
+
+    // The user image associated with the app
     val userImage: Drawable,
+
+    // The package name of the app
     val packageName: String?="",
 
 )
+
+// Data classes in Kotlin automatically generate useful functions such as 'equals', 'hashCode' and 'copy'
+// These functions help with comparing instances, creating new instances with modified values and more
