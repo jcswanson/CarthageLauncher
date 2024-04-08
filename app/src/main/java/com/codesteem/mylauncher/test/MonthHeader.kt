f8fcfd3ba853bce89132c9d85fc87624a6728a3a
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:55 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/test/MonthHeader.kt
+++ app/src/main/java/com/codesteem/mylauncher/test/MonthHeader.kt
@@ -1,7 +1,15 @@
-package com.codesteem.mylauncher.test
+// The MonthHeader class represents a header for a month in a calendar or similar display.
+// It extends the MonthItem abstract class and overrides the 'name' and 'type' properties.
 
+// The 'name' property specifies the name of the month (e.g. "January", "February", etc.).
+// This property is required and must be provided when creating a new instance of MonthHeader.
 class MonthHeader(override val name: String, override val openCounter: Int = 0) : MonthItem {
 
+    // The 'type' property is a read-only property that specifies the type of MonthItem.
+    // For MonthHeader, the type is set to MonthItem.MonthItemType.HEADER.
     override val type: MonthItem.MonthItemType
         get() = MonthItem.MonthItemType.HEADER
 }
+
+// The 'openCounter' property is an optional property that specifies the number of items currently open
+// for the given month. By default, it is set to 0.
