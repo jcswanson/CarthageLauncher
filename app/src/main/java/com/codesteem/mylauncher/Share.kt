e7153ad47b073732c2d23cd9d8124cfc8377a435
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:30 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/Share.kt
+++ app/src/main/java/com/codesteem/mylauncher/Share.kt
@@ -1,6 +1,9 @@
-package com.codesteem.mylauncher
-
+// Share data class
+// This class represents a share object that contains an app and data to be shared.
 data class Share(
-    val app: String,
-    val data: String
+    // The name of the app to share.
+    val app: String = "",
+    
+    // The data to be shared by the app.
+    val data: String = ""
 )
