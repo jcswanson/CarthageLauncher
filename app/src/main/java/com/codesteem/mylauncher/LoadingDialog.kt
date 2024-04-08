4e4600e6ed72d1e735d9f1ed48f8f595771ea862
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:33 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/LoadingDialog.kt
+++ app/src/main/java/com/codesteem/mylauncher/LoadingDialog.kt
@@ -8,34 +8,42 @@ import android.os.Bundle
 import android.view.Window
 import com.codesteem.mylauncher.databinding.LoadingDialogBinding
 
+/**
+ * A custom loading dialog class for displaying a loading animation with an optional title.
+ * Users can set an OnActionListener to handle the OK and Cancel button clicks.
+ */
 class LoadingDialog(
-    context: Context,
-    private val title: String?
-): Dialog(context) {
-    private lateinit var binding: LoadingDialogBinding
-    private var mListener: OnActionListener? = null
+    context: Context, // Context of the calling component
+    private val title: String? // Optional title for the dialog
+) : Dialog(context) {
+    private lateinit var binding: LoadingDialogBinding // View binding for the dialog layout
+    private var mListener: OnActionListener? = null // Listener for button clicks
 
+    /**
+     * Dialog creation method, inflates the layout, sets the content view, and configures the dialog window.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = LoadingDialogBinding.inflate(layoutInflater)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(binding.root)
+        binding = LoadingDialogBinding.inflate(layoutInflater) // Inflate the dialog layout
+        requestWindowFeature(Window.FEATURE_NO_TITLE) // Remove the default dialog title
+        setContentView(binding.root) // Set the content view using the inflated layout
+
+        // Calculate and set the dialog width to 90% of the screen width
         val width = context.resources.displayMetrics.widthPixels * 0.90
         binding.root.layoutParams.width = width.toInt()
-        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
+        // Set the title if provided
         if (title != null) {
             binding.title.text = title
         }
+
+        // Transparent background for the dialog window
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
 
+    /**
+     * Interface for handling button clicks in the dialog
+     */
     interface OnActionListener {
-        fun onOK()
-        fun onCancel()
-    }
-
-    fun setOnActionListener(listener: OnActionListener?) {
-        mListener = listener
-    }
-
-}
+        fun onOK() // Method for handling the OK button click
+        fun onCancel() // Method for handling the C
