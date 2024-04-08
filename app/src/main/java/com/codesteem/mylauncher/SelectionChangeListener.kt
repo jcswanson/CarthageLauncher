b421deb8cb67b82c79bda44e92771c15feffcdeb
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:31 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/SelectionChangeListener.kt
+++ app/src/main/java/com/codesteem/mylauncher/SelectionChangeListener.kt
@@ -1,5 +1,9 @@
-package com.codesteem.mylauncher
-
+// Interface for listening to changes in user selection within the app.
+// Implementing classes should provide an implementation for the `onSelectionChanged` method.
 interface SelectionChangeListener {
-    fun onSelectionChanged(selectedCount: Int,packageName:String)
+
+    // Called when the user's selection has changed within the app.
+    //  `selectedCount` represents the number of items currently selected,
+    //  while `packageName` identifies the package or context in which the selection occurred.
+    fun onSelectionChanged(selectedCount: Int, packageName: String)
 }
