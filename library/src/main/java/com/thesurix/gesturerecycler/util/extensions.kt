87812a0e0d9a90c66d275b48e518a04f8448eb51
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:48 -0700

    comment

--- library/src/main/java/com/thesurix/gesturerecycler/util/extensions.kt
+++ library/src/main/java/com/thesurix/gesturerecycler/util/extensions.kt
@@ -1,19 +1,29 @@
 package com.thesurix.gesturerecycler.util
 
 /**
- * Swaps two items inside [MutableList]
- * @param firstIndex first index to swap
- * @param secondIndex second index to swap
+ * This function swaps two items inside a MutableList.
+ * It takes two integer arguments, `firstIndex` and `secondIndex`,
+ * which represent the indices of the items to be swapped.
+ *
+ * @param firstIndex the index of the first item to swap
+ * @param secondIndex the index of the second item to swap
  */
 fun <T> MutableList<T>.swap(firstIndex: Int, secondIndex: Int) {
+    // Temporarily store the value of the first index
     val tmp = this[firstIndex]
+    
+    // Set the value of the first index to the value of the second index
     this[firstIndex] = this[secondIndex]
+    
+    // Set the value of the second index to the temporarily stored value
     this[secondIndex] = tmp
 }
 
 /**
- * Returns data offset based on header state.
- * @param headerEnabled header flag
- * @return data offset
+ * This function returns the data offset based on the state of the header.
+ * It takes a boolean argument, `headerEnabled`, which indicates whether the header is enabled or not.
+ *
+ * @param headerEnabled a flag indicating whether the header is enabled (true) or not (false)
+ * @return the data offset
  */
 fun getDataOffset(headerEnabled: Boolean) = if (headerEnabled) -1 else 0
