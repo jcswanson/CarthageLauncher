530f0b0ff79e7cfce1a4accb59701720a4ad7a17
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:46 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/database/NotificationDao.kt
+++ app/src/main/java/com/codesteem/mylauncher/database/NotificationDao.kt
@@ -1,28 +1,46 @@
-package com.codesteem.mylauncher.database
-
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-
 @Dao
 interface NotificationDao {
 
+    /**
+     * Gets the [NotificationEntity] object with the given [notificationId] from the database.
+     *
+     * @param notificationId The unique identifier of the notification to retrieve.
+     * @return The [NotificationEntity] object with the given [notificationId], or null if no such object exists.
+     */
     @Query("SELECT * FROM notification_table WHERE id = :notificationId")
     suspend fun getNotificationById(notificationId: String): NotificationEntity?
 
+    /**
+     * Deletes the [NotificationEntity] object with the given [notificationId] from the database.
+     *
+     * @param notificationId The unique identifier of the notification to delete.
+     */
     @Query("DELETE FROM notification_table WHERE id = :notificationId")
     suspend fun deleteNotificationById(notificationId: String)
 
+    /**
+     * Inserts the given [notification] into the database, replacing any existing notification with the same id.
+     *
+     * @param notification The [NotificationEntity] object to insert.
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNotification(notification: NotificationEntity)
 
+    /**
+     * Gets a list of all prioritized [NotificationEntity] objects from the database.
+     *
+     * @return A list of all prioritized [NotificationEntity] objects.
+     */
     @Query("SELECT * FROM notification_table WHERE isPrioritized = 1")
     suspend fun getPrioritizedNotifications(): List<NotificationEntity>
 
+    /**
+     * Gets a list of all [NotificationEntity] objects from the database, sorted by timestamp in descending order.
+     *
+     * @return A list of all [NotificationEntity] objects, sorted by timestamp in descending order.
+     */
     @Query("SELECT * FROM notification_table ORDER BY timestamp DESC")
     suspend fun getAllNotifications(): List<NotificationEntity>
 
-    @Query("DELETE FROM notification_table")
-    suspend fun deleteAllNotifications()
-}
+    /**
+     *
