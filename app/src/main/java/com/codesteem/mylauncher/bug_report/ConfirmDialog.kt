fcce4aeab0775021a09a976794a5f06b7e3dba31
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:41 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/bug_report/ConfirmDialog.kt
+++ app/src/main/java/com/codesteem/mylauncher/bug_report/ConfirmDialog.kt
@@ -9,36 +9,46 @@ import android.view.Window
 import com.codesteem.mylauncher.databinding.ConfirmDialogBinding
 import com.codesteem.mylauncher.databinding.LoadingDialogBinding
 
+// Custom ConfirmDialog class extending Dialog for displaying a confirmation dialog
 class ConfirmDialog(
-    context: Context
-): Dialog(context) {
-    private lateinit var binding: ConfirmDialogBinding
-    private var mListener: OnActionListener? = null
+    context: Context // Context object passed during initialization
+) : Dialog(context) {
+    private lateinit var binding: ConfirmDialogBinding // View binding for the dialog layout
+    private var mListener: OnActionListener? = null // Interface for handling user actions
 
+    // Overriding onCreate method for dialog initialization
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Inflating the dialog layout using view binding
         binding = ConfirmDialogBinding.inflate(layoutInflater)
+
+        // Requesting no title for the dialog
         requestWindowFeature(Window.FEATURE_NO_TITLE)
+
+        // Setting the content view with the inflated layout
         setContentView(binding.root)
+
+        // Calculating the width of the dialog to cover 90% of the screen width
         val width = context.resources.displayMetrics.widthPixels * 0.90
+
+        // Setting the width of the dialog
         binding.root.layoutParams.width = width.toInt()
+
+        // Setting the background of the dialog window to transparent
         window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
+        // Setting click listeners for yes and no buttons
         binding.yes.setOnClickListener {
-            mListener?.onOK()
+            mListener?.onOK() // Calling onOK method of the listener
         }
         binding.no.setOnClickListener {
-            mListener?.onCancel()
+            mListener?.onCancel() // Calling onCancel method of the listener
         }
     }
 
+    // Interface for handling user actions
     interface OnActionListener {
-        fun onOK()
-        fun onCancel()
+        fun onOK() // Method for handling OK action
+        fun onCancel() // Method for handling Cancel action
     }
-
-    fun setOnActionListener(listener: OnActionListener?) {
-        mListener = listener
-    }
-
-}
