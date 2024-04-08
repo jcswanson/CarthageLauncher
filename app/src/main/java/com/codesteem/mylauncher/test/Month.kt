c14bc934d1a27e6121ce9e009633faf5a7c3209e
John Christian Swanson <johncswan@hotmail.com>
2024-04-08 16:09:59 -0700

    comment

--- app/src/main/java/com/codesteem/mylauncher/test/Month.kt
+++ app/src/main/java/com/codesteem/mylauncher/test/Month.kt
@@ -1,13 +1,28 @@
 package com.codesteem.mylauncher.test
 
-
+// Importing necessary classes and interfaces
 import android.graphics.drawable.Drawable
 import androidx.annotation.DrawableRes
 
-class Month(override val name: String, val drawableId: Drawable, val packageName: String, var counterNotification: Int?=0,
-            override val openCounter: Int
+// Defining Month class that implements MonthItem interface
+class Month(
+    // Overriding name property from MonthItem interface
+    override val name: String,
+
+    // drawableId property to store the Drawable object
+    val drawableId: Drawable,
+
+    // packageName property to store the package name
+    val packageName: String,
+
+    // counterNotification property to store the counter notification
+    var counterNotification: Int? = 0, // Nullable integer to allow for optional notification count
+
+    // openCounter property to store the open counter
+    override val openCounter: Int // Inherited from MonthItem interface
 ) : MonthItem {
 
+    // Overriding type property from MonthItem interface
     override val type: MonthItem.MonthItemType
         get() = MonthItem.MonthItemType.MONTH
 }
